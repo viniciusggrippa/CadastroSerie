c885b41276147bc89410fdc7de9df732b80cd51c
Vinicius Grippa <viniciusdegoesgrippa@outlook.com>
2021-06-07 19:05:26 -0300

    Adicionado um repositório base

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using Dio.Series.Interfaces;
+
+namespace Dio.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
